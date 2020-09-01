797d9544df0a1c25109cc68d1d6c579dd58caa98
Jorge Carrión <jorge_28cs@hotmail.com>
2020-08-31 22:30:44 -0500

    first commit

---
+++ Challenge_person_POO.js
@@ -0,0 +1,18 @@
+class Person {
+  constructor(name, lastName, age, gender) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+  walk(){
+    //code
+  }
+  eat() {
+    //code
+  }
+  speak() {
+    //code
+  }
+}
+
